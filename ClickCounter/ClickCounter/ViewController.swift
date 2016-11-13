4dca0fd8f966c24848b58b4223143cec163079fc
Damo de Lemos <lemoss@amazon.com>
2016-11-12 20:34:39 -0600

    Using storyboard

--- ClickCounter/ClickCounter/ViewController.swift
+++ ClickCounter/ClickCounter/ViewController.swift
@@ -11,30 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     var count = 0
-    var label:UILabel!
+    @IBOutlet var label:UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //Label
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
-        label.text = "0"
-        
-        self.view.addSubview(label)
-        self.label = label
-        
-        //Button
-        let button = UIButton()
-        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
-        button.setTitle("Click", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        self.view.addSubview(button)
-        
-        button.addTarget(self, action: #selector(self.incrementCount), for: .touchUpInside)
     }
     
-    func incrementCount() {
+    @IBAction func incrementCount() {
         self.count += 1
         self.label.text = "\(self.count)"
     }
